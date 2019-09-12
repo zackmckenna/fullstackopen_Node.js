829e1f34b06e5b1cdb5bedec98bd1fee53dc6ae1
Zack McKenna <zackmckenna@gmail.com>
2019-09-12 16:06:25 -0400

    Added build

---
+++ build/precache-manifest.74cfc23b19fc0477352e5579aaefe8a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3557c1adb57fe309f538d8490e7de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2938595df47e5cc7afed",
+    "url": "/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "4ff130d75b39be6ba61f",
+    "url": "/static/js/2.81ae3ceb.chunk.js"
+  },
+  {
+    "revision": "2938595df47e5cc7afed",
+    "url": "/static/js/main.273915af.chunk.js"
+  },
+  {
+    "revision": "cb8776fd9024a7da0bb8",
+    "url": "/static/js/runtime~main.311cb9fb.js"
+  }
+]);
